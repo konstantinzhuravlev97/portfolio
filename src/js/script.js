112e4d2af2938f8549dfb9fbb0b6abb7fabbab8e
Kostya <example@gmail.com>
2025-01-22 16:45:50 +0600

    add sections resume and skills

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__percent-number'),
+      lines = document.querySelectorAll('.skills__percent-scale_complete');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
